5e679a58b5a25526b4732c2fb4b33c4ff46125bb
Kevin Abey Issac <kevinabey04@gmail.com>
2024-04-21 15:35:18 +0530

    Clean Up

--- background.js
+++ background.js
@@ -1,32 +1,19 @@
 console.log("background.js started");
-import { GoogleGenerativeAI } from "https://cdn.jsdelivr.net/npm/@google/generative-ai/+esm";
+import { GoogleGenerativeAI } from "https://cdn.jsdelivr.net/npm/@google/generative-ai/+esm"; //Import-map to access gemini api
 
-chrome.runtime.onMessage.addListener(
+chrome.runtime.onMessage.addListener( 
     async(message, sender, sendResponse) => {
         console.log("background.js if reached");
-        if (message.text) { // Changed from message.websiteText to message.text
+        if (message.text) { //
             console.log("Website text received in background.js:", message.text);
-            console.log("WORKS2");
-            const genAI = new GoogleGenerativeAI("AIzaSyCQKV_ZsuLNR7Ka2ElUPlWSWHFx0XRBKR4");
+            const genAI = new GoogleGenerativeAI("[API KEY HERE]"); //use gemini api key here available at: https://aistudio.google.com/app/apikey
             const model = genAI.getGenerativeModel({ model: "gemini-pro"});
             const prompt = `const prompt = Ascertain whether the content provided below is factually incorrect or misinformation or probably reliable information. Give the output as a JSON file text without addtional formatting. JSON FORMAT - (Title: [title of the news] Reliability: [percentage of credibility ranging from 0% to 100%] Status: [Probably False,Probably True or Uncertain] Description: [an explaination for giving the status]) CONTENT -(${message.text})`;
             const result = await model.generateContent(prompt);
             const response = await result.response;
-            const text = await response.text(); // Await the Promise
-            console.log("STUFF: \n");
+            const text = await response.text(); //gemini gives output in json format
             console.log(text);
-            /*const jsonObject =  JSON.parse(text);
-            const finalText = jsonObject.Description;
-            console.log("IMP!!:\n"+finalText);*/
-
-            
-            /*chrome.tabs.query({active: true, currentWindow: true}, 
-                tabs =>  
-                    chrome.tabs.sendMessage(tabs[0].id,{msg: finalText})
-
-            );*/
-                chrome.runtime.sendMessage({msg: text});
-            
+            chrome.runtime.sendMessage({msg: text});
         }
     }
 );
--- popup.js
+++ popup.js
@@ -1,66 +1,53 @@
-// Listen for clicks on the "Read Website Text" button
-document.getElementById('readButton').addEventListener('click', () => {
-                document.querySelector('body').classList.add('body');;
-        document.getElementById('descriptionBox').innerText = "LOADING";
-     document.getElementById('reliabilityPercentage').innerText="LOADING";
-document.getElementById('statusText').innerText= "LOADING";
-        document.getElementById('Status').innerHTML= '<i class="fa-solid fa-circle-question"></i>';
+document.getElementById('readButton').addEventListener('click', () => { //button listener and update
+	document.querySelector('body').classList.add('body');;
+	document.getElementById('reliabilityPercentage').innerText="loading..";
+	document.getElementById('statusText').innerText= "loading...";
+	document.getElementById('descriptionBox').innerText = "description will be generated soon...";
+	document.getElementById('Status').innerHTML= '<i class="fa-solid fa-circle-question"></i>';
 
-  // Get the active tab
   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    // Execute a script in the active tab to read the contents of the body
-    chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id },
-      function: () => {
-        // Read the contents of the body and store it in a variable
-        const websiteText = document.body.innerText;
-        // Send a message to the popup with the website text
-        chrome.runtime.sendMessage({ text: websiteText });
-        console.log(websiteText);
-        console.log("popup.js finished");
-      }
-    });
+	chrome.scripting.executeScript({
+	  target: { tabId: tabs[0].id },
+	  function: () => {
+		const websiteText = document.body.innerText;
+		chrome.runtime.sendMessage({ text: websiteText }); //message sent to background.js
+		console.log(websiteText);
+		console.log("popup.js finished");
+	  }
+	});
   });
 });
 
-chrome.runtime.onMessage.addListener(async (resp) => {
-    if(resp) {
-        console.log("RESPPP");
-    run(resp);
-    }
+chrome.runtime.onMessage.addListener(async (resp) => { //recieves json from background.js
+	if(resp) {
+		run(resp);
+	}
 });
 
 async function run(resp) {
-    const response = await resp.msg;
-    if(response) {
-        try {
-        const jsonObject =  JSON.parse(response);
-
-        const finalText = jsonObject.Description;
-        const status = jsonObject.Status;
-        document.getElementById('reliabilityPercentage').innerText = jsonObject.Reliability;
-        document.getElementById('statusText').innerText = status;
-        if(status === "Probably True") {
-            document.getElementById('Status').innerHTML = '<i class="fa-solid fa-square-check"></i>';
-            document.documentElement.style.setProperty('--bg-color', '#0eb87b'); // Green background
-            document.documentElement.style.setProperty('--secondary-color', '#0cffa1'); // Black text color
-        }
-        else if(status === "Probably False") {
-            document.getElementById('Status').innerHTML = '<i class="fa-solid fa-square-xmark"></i>';
-            document.documentElement.style.setProperty('--bg-color', '#ff611e'); // Red background
-            document.documentElement.style.setProperty('--secondary-color', '#cc4d18'); // Black text color
-        }
-	else {
-		document.getElementById('Status').innerHTML = '<i class="fa-solid fa-circle-question"></i>';
-
-    }
-        document.getElementById('descriptionBox').innerText = finalText;
-    } catch(err) {
-            console.log(err);
-document.getElementById('descriptionBox').innerText = "Error in json please retry.";
-
-
-        }
-}
-
-}
+	const response = await resp.msg; 
+	if(response) {
+		try { //there may be errors in json file return by gemini
+			const jsonObject =  JSON.parse(response);
+			const finalText = jsonObject.Description;
+			const status = jsonObject.Status;
+			document.getElementById('reliabilityPercentage').innerText = jsonObject.Reliability;
+			document.getElementById('statusText').innerText = status;
+			if(status === "Probably True") {
+				document.getElementById('Status').innerHTML = '<i class="fa-solid fa-square-check"></i>';
+				document.documentElement.style.setProperty('--bg-color', '#0eb87b');
+				document.documentElement.style.setProperty('--secondary-color', '#0cffa1');
+			}
+			else if(status === "Probably False") {
+				document.getElementById('Status').innerHTML = '<i class="fa-solid fa-square-xmark"></i>';
+				document.documentElement.style.setProperty('--bg-color', '#ff611e');
+				document.documentElement.style.setProperty('--secondary-color', '#cc4d18');
+			}
+			else {
+				document.getElementById('Status').innerHTML = '<i class="fa-solid fa-circle-question"></i>';
+			}
+			document.getElementById('descriptionBox').innerText = finalText;
+	} catch(err) {
+			console.log(err);
+	document.getElementById('descriptionBox').innerText = "Error in json please retry.";
+}}}
