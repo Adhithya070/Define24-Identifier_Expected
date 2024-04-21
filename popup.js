26d23a06be6194ae8967df26d5a02d3f94857138
Shivsay <shivsa543@gmail.com>
2024-04-21 10:05:23 +0530

    popup update

--- popup.js
+++ popup.js
@@ -1,6 +1,11 @@
 // Listen for clicks on the "Read Website Text" button
 document.getElementById('readButton').addEventListener('click', () => {
         document.getElementById('descriptionBox').innerText = "LOADING";
+     document.getElementById('reliabilityPercentage').innerText="LOADING";
+document.getElementByI('statusText').innerText= "LOADING";
+        document.getElementById('Status').innerHTML= "";
+
+    //document.getElementById('StatusIcon').innerHTML= '<i class="fa-solid fa-circle-question"></i>';
   // Get the active tab
   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
     // Execute a script in the active tab to read the contents of the body
@@ -22,6 +27,17 @@ chrome.runtime.onMessage.addListener(async (resp) => {
     //console.log(resp.msg);
     const response = await resp.msg;
     if(response) {
-  document.getElementById('descriptionBox').innerText = response;
+        const jsonObject =  JSON.parse(response);
+        const finalText = jsonObject.Description;
+        const status = jsonObject.Status;
+        document.getElementById('reliabilityPercentage').innerText = jsonObject.Reliability;
+        document.getElementById('statusText').innerText = status;
+        if(status === "True") {
+            document.getElementById('Status').innerHTML = '<i class="fa-solid fa-square-check"></i>';
+        }
+        else if(status === "Uncertain") {
+            document.getElementById('Status').innerHTML = '<i class="fa-solid fa-circle-question"></i>';
+        }
+  document.getElementById('descriptionBox').innerText = finalText;
     }
 });
